27e4605b3e883506743b83b850027465f11a4d16
Demir Subasic <subasicd2000@gmail.com>
2023-12-21 02:33:53 +0100

    changing background and fixed some changes on navbar, added other section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/navbar";
-import navbar from "./components/navbar";
+import Main from "./components/main";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Main />
     </div>
   );
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main.scss'
+
+const Main = () => {
+  return (
+    <div className='main'>main</div>
+  )
+}
+
+export default Main
